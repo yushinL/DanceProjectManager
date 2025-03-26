0c3cad833879da41ccfbecd2406c10b19b8ea861
yushinliang <liangyuxin0406@berkeley.edu>
2025-03-25 19:32:14 -0700

    login page generate half done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,61 @@
-import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import Task from "./pages/Task"
+import Users from "./pages/Users"
+import Trash from "./pages/Trash"
+import TaskDetails from "./pages/TaskDetails"
+import {Routes, Route, Navigate, Outlet, useLocation} from 'react-router-dom'
+import {Toaster} from "sonner"
+
+function Layout() {
+  const user = ""
+  const location = useLocation()
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-white sticky top-0 hidden md:block'>
+        {/* <Sidebar /> */}
+      </div>
+
+      {/* <MobileSidebar /> */}
+      
+      <div className='flex-1 overflow-y-auto'>
+        {/* <Navbar/> */}
+
+        <div className='p-4 2x1:px-10'>
+          <Outlet />
+        </div>
+
+      </div>
+    </div>
+  ) : (
+    // direct to login page if user not exists
+    <Navigate to="/log-in" state={{from: location}} replace />
+  )
+}
 
 function App() {
-  return <Login />;
+  return (
+    <main className='w-full min-h-screen bg-[#f3f4f6]'>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path='/' element={<Navigate to="/dashboard" />} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/tasks' element={<Task />} />
+          <Route path='/completed/:status' element={<Task />} />
+          <Route path='/in-progress/:status' element={<Task />} />
+          <Route path='/todo/:status' element={<Task />} />
+          <Route path='/team' element={<Users />} />
+          <Route path='/trash' element={<Trash />} />
+          <Route path='/task/:id' element={<TaskDetails />} />
+        </Route>
+        
+        <Route path='/log-in' element={<Login />} />
+      </Routes>
+
+      <Toaster richColors /> 
+    </main>
+  )
+
 }
 
 export default App;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {useForm} from 'react-hook-form';
+import {useNavigate} from 'react-router-dom';
 
 const Login = () => {
+  const user = "";
+  const {
+    register, 
+    handleSubmit, 
+    formState: { errors }
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  // navigate to dashboard when logged in
+  useEffect(() => {
+    user && navigate("/dashboard");
+  }, [user]);
+
   return (
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-    </h1>
+    <div className='w-full min-h-screen flex items-center justify-center justify-center flex-col lg:flex-row bg-[#f3f4f6]'>
+      <div className='w-full md:w-auto flex gap-0 md:gap-40 flex-col md:flex-row items-center justify-center'>
+        {/* left side */}
+        <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
+          <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
+            <span className='flex gap-1 py-1 px-3 border rounded-full text-sm md:text-base dark:border-gray-700 dark:text-blue-400 border-gray-300 text-gray-600'>
+              Mange the dance projects all in once!
+            </span>
+            <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-blue-700'>
+              <span>Cloud-Based</span>
+              <span>Task Manager</span>
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
